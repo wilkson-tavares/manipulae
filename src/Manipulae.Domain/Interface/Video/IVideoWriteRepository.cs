b2d08e504c634ff551f1513a917afe270cc4911c
Wilkson Tavares <wilkson.tavares@gmail.com>
2025-01-28 21:26:11 -0300

    feat: Implementação do projeto Domain

---
+++ src/Manipulae.Domain/Interface/Video/IVideoWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Manipulae.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulae.Domain.Interface.Video
+{
+    public interface IVideoWriteRepository
+    {
+        Task Add(VideoEntity video);
+    }
+}
---
+++ src/Manipulae.Domain/Responses/RegisteredVideoResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulae.Domain.Responses
+{
+    public class RegisteredVideoResponse
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public TimeSpan Duracao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public string Canal { get; set; } = string.Empty;
+    }
+}
